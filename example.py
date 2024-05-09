25b224ceaa0849f5d0f6ec234f211cc57bec1f40
MochamadAbdulRouf <163649906+MochamadAbdulRouf@users.noreply.github.com>
2024-05-09 10:34:27 +0700

    Add files via upload

---
+++ example.py
@@ -0,0 +1,54 @@
+#usia = 12
+
+#if usia == 0:
+#   print('belum lahir')
+#elif usia >= 8 and usia <= 13:
+#    print('Lu bochil rada remaja')
+
+#nama = "rouf"
+#usia = 16
+#tinggi_badan = 170
+
+#print('nama saya rouf tinggi saya adalah ' + str(tinggi_badan))
+
+#a = input('Nomer berapa ? ')
+#b = input('Nomer Kedua? ')
+#c = int(a) // int(b)
+
+#print('hasilnya adalah ' + str(c))
+
+#length = 10; width = 5
+#area = length * width
+#print(area)
+
+
+#a = luas_bangunan = 20 
+#b = tinggi_bangunan = 30
+#c = a * b 
+#print(c) 
+
+#!/usr/bin/env python3 
+#url = "www.redhat.com" 
+#hasil = url.startswith("www") 
+#print(hasil) 
+#print(url.endswith(".org")) 
+#new_url = url.upper( ) 
+#print("ASLI", url, "KEMBALI")
+
+#url = 'google.org'
+#hasil = url.startswith(".org")
+#print(hasil)
+#print(url.endswith(";org"))
+#new_url = url.upper()
+#print('asli', url, 'kembali')
+
+#int()	str()	chr()	oct()
+#float()	ord()	hex()	bin()
+#, int(), float()dan str()#merupakan 
+#konstruktor yang menginisialisasi objek, bukan pemanggilan fungsi.
+
+a = input('please berikan sebuah kata ')
+b = input('Respon terakhir ?')
+c = a + b
+
+print(c)
---
+++ mengulang.py
@@ -0,0 +1,6 @@
+a = input('nomer pertama ?')
+b = input('nomer kedua ?')
+
+x = int(a) * int(b)
+
+print('hasilnya adalah '+ str(x)) 
